6bb3f2092bbad3ac708ad7898c9ccacb6bb350bf
Taras Bilous <tbilous@yahoo.com>
2016-10-13 14:46:51 +0300

    fixed notes

--- db/migrate/20161012144133_create_answers.rb
+++ db/migrate/20161012144133_create_answers.rb
@@ -1,13 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.integer :question_id
-
+      t.belongs_to :question, index: true, foreign_key: true
       t.text :body
 
       t.timestamps null: false
     end
-    add_foreign_key :answers, :questions
-    add_index :answers, [:question_id, :created_at]
+    add_index :answers, :created_at
   end
 end
