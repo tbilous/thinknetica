d884ee742ad257c320abf3848a2d389a1983f2e9
Taras Bilous <tbilous@yahoo.com>
2016-10-13 15:14:08 +0300

    check houndci-bot notes

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-
   it { should validate_presence_of :question_id }
   it { should belong_to(:question) }
   it { should validate_length_of(:body).is_at_least(60) }
-
 end
