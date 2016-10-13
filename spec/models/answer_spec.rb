d19324abd98ca7f40a52dada4a44314a8dcbcb86
Taras Bilous <tbilous@yahoo.com>
2016-10-13 18:32:58 +0300

    order test like model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of :question_id }
-  it { should belong_to(:question) }
   it { should validate_length_of(:body).is_at_least(60) }
+  it { should belong_to(:question) }
 end
