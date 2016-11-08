b125f40c7aeab640b6405cd14e3de91ea2546169
Taras Bilous <tbilous@yahoo.com>
2016-11-08 20:35:00 +0200

    devise forms, js utils, render_errors_for

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,21 @@ module ApplicationHelper
     end
     nil
   end
+
+  def shallow_resource(*args)
+    if args.last.persisted?
+      args.last
+    else
+      args
+    end
+  end
+
+  def render_flash
+    javascript_tag("App.flash = JSON.parse(" "'#{j({ success: flash.notice, error: flash.alert }.to_json)}'" ");")
+  end
+
+  def render_errors_for(resource)
+    return unless resource.errors.any?
+    flash.now.alert = resource.errors.full_messages.join(", ")
+  end
 end
