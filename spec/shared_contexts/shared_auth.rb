847228fb1eed828229ae04f0b05b9c54b6be2c19
Taras Bilous <tbilous@yahoo.com>
2016-11-13 01:22:24 +0200

    answer - comments test model & controller is OK

---
+++ spec/shared_contexts/shared_auth.rb
@@ -0,0 +1,13 @@
+shared_context 'authorized user', auth: true do
+	before { sign_in(user) }
+end
+
+shared_context 'unauthorized user', unauth: true do
+	before { sign_out(user) }
+end
+
+shared_context 'non owner user', auth: true do
+  include_context 'users'
+  before { sign_in(john) }
+end
+
---
+++ spec/shared_examples/controllers/invalid_examples.rb
@@ -0,0 +1,28 @@
+shared_examples "invalid params" do |message, model: nil, code: 403|
+  context message do
+    if model
+      it "doesn't create #{model}" do
+        expect { subject }.not_to change(model, :count)
+      end
+    end
+
+    if code
+      it "returns #{code}" do
+        expect(subject).to have_http_status(code)
+      end
+    end
+  end
+end
+
+shared_examples "unauthorized user request" do |message, **hargs|
+  include_context "unauthorized user"
+  include_examples "invalid params", message, hargs.merge(code: 401)
+end
+
+shared_examples "unauthorized user destroy" do
+  include_context "unauthorized user"
+end
+
+shared_examples "non owner" do
+  include_context "non owner user"
+end
