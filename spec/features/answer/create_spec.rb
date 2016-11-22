bd43d36fbc53e47810ee73f0b5eff34e619dbdcd
Taras Bilous <tbilous@yahoo.com>
2016-11-22 17:12:49 +0200

    answer create & delete  test OK

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -9,40 +9,71 @@ feature 'Create answer', %q{
   let(:user) { create(:user) }
   let!(:question) { create(:question, user: user) }
   let(:answer_params) { attributes_for(:answer) }
+  context 'as user', :js do
+    scenario 'Authenticated user creates answer with proper data' do
+      login_as(user)
+      visit question_path(question)
 
-  scenario 'Authenticated user creates answer with proper data', js: true do
-    login_as(user)
-    visit question_path(question)
+      fill_in 'answer_body', with: answer_params[:body]
+      # sleep(inspection_time=5)
+      within '#new_answer' do
+        click_button('submit')
+      end
 
-    fill_in 'answer_body', with: answer_params[:body]
-    # sleep(inspection_time=5)
-    within '#new_answer' do
-      click_button('submit')
+      within '.answer-rendered' do
+        expect(page).to have_content answer_params[:body]
+      end
+
+      expect(current_path).to eq question_path(question)
     end
 
-    within '.answer-rendered' do
-      expect(page).to have_content answer_params[:body]
+    scenario 'all users see new question in real-time' do
+      Capybara.using_session('author') do
+        login_as(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('guest') do
+        visit question_path(question)
+      end
+
+      Capybara.using_session('author') do
+
+        fill_in 'answer_body', with: answer_params[:body]
+        within '#new_answer' do
+          click_button('submit')
+        end
+
+        within '.answer-rendered' do
+          expect(page).to have_content answer_params[:body]
+        end
+
+      end
+      Capybara.using_session('guest') do
+        within '.answer-rendered' do
+          expect(page).to have_content answer_params[:body]
+        end
+      end
     end
 
-    expect(current_path).to eq question_path(question)
+    scenario 'Authenticated user tries to create answer with invalid data', js: true do
+      login_as(user, scope: :user)
+      visit question_path(question)
+
+      fill_in 'answer_body', with: 'a' * 2
+
+      within '#new_answer' do
+        click_button('submit')
+      end
+
+      expect(current_path).to eq question_path(question)
+    end
   end
+  context 'as guest', :js do
+    scenario 'Non-authenticated user tries to create answer' do
+      visit question_path(question)
 
-  scenario 'Authenticated user tries to create answer with invalid data', js: true do
-    login_as(user, scope: :user)
-    visit question_path(question)
-
-    fill_in 'answer_body', with: 'a' * 2
-
-    within '#new_answer' do
-      click_button('submit')
+      expect(page).to_not have_content 'Add answer'
     end
-
-    expect(current_path).to eq question_path(question)
-  end
-
-  scenario 'Non-authenticated user tries to create answer', js: true do
-    visit question_path(question)
-
-    expect(page).to_not have_content 'Add answer'
   end
 end
