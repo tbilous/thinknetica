e7f3c6092d014b9ac920f08a220994f132212fc4
Taras Bilous <tbilous@yahoo.com>
2016-11-28 22:56:45 +0200

    all test facebook OK

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: auth.provider.capitalize) if is_navigational_format?
+      # set_flash_message(:notice, :success, kind: auth.provider.capitalize) if is_navigational_format?
     end
   end
 
