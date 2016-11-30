9178d9f4a558ee6fd84d355121c786d5d6f1754d
Taras Bilous <tbilous@yahoo.com>
2016-11-30 21:04:15 +0200

    change flash mail-edit view

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,19 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :get_user, only: [:edit_email, :update_email]
+
   def edit_email; end
 
-  def update_email
-    @user = User.find(session['devise.new_user_id'])
 
+  def update_email
     if @user && @user.update(email: params['email'])
       redirect_to root_path
       set_flash_message(:notice, :send_instructions) if is_navigational_format?
     else
       render 'edit_email'
     end
+  end
+
+  def get_user
+    @user = User.find(session['devise.new_user_id'])
   end
 end
