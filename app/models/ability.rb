a2170b22d010c451759353ec9f2bf249f6f2f0d7
Taras Bilous <tbilous@yahoo.com>
2016-12-05 15:48:37 +0200

    add Admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,28 +7,38 @@ class Ability
     alias_action :update, :destroy, to: :modify
     alias_action :vote_plus, :vote_minus, :vote_cancel, to: :vote
 
-    user ? user_abilities(user) : guest_abilities
+    if user
+      user.admin? ? admin_abilities : user_abilities
+    else
+      guest_abilities
+    end
   end
+
 
   def guest_abilities
     can :read, :all
   end
 
-  def user_abilities(user)
+
+  def user_abilities
     guest_abilities
 
     can :create, [Question, Answer, Comment]
 
-    can :modify, [Question, Answer, Comment], user_id: user.id
+    can :modify, [Question, Answer, Comment], user_id: @user.id
 
-    can :destroy, [Attachment], attachable: { user_id: user.id }
+    can :destroy, [Attachment], attachable: { user_id: @user.id }
 
-    can :assign_best, Answer, question: { user_id: user.id }
+    can :assign_best, Answer, question: { user_id: @user.id }
 
     can :vote, [Question, Answer] do |resource|
-      resource.user_id != user.id
+      resource.user_id != @user.id
     end
 
     can :me, User
+  end
+
+  def admin_abilities
+    can :manage, :all
   end
 end
