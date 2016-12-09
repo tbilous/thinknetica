d50017dabbe8c631e114b7dc827c2773ddd22616
Taras Bilous <tbilous@yahoo.com>
2016-12-09 22:02:07 +0200

    questions tes refactoring

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -7,11 +7,10 @@ RSpec.describe QuestionsController, type: :controller do
 
   before :each do
     @request.env['devise.mapping'] = Devise.mappings[:user]
-    @other_user = user
-    @user = john
   end
 
-  let(:question) { create(:question, user: @user) }
+  let(:question) { create(:question, user: user) }
+  let(:answer) { create(:answer, user: user) }
 
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
@@ -25,63 +24,25 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :index
     end
 
-    context 'user is authorized' do
-      login_user
-
-      it 'assign New Question' do
-        expect(assigns(:question)).to be_a_new(Question)
-      end
-      it 'assign Attach' do
-        expect(assigns(:question).attachments.first).to be_a_new(Attachment)
-      end
+    it_behaves_like 'when user is authorized' do
+      it { expect(assigns(:question)).to be_a_new(Question) }
+      it { expect(assigns(:question).attachments.first).to be_a_new(Attachment) }
     end
   end
 
   describe 'GET #show' do
     before { get :show, params: { id: question.id } }
 
-    it 'assign question' do
-      expect(assigns(:question)).to eq question
-    end
-
     it 'render the show template' do
       expect(response).to render_template :show
     end
-    context 'user is not authorized', js: true do
-      before { get :edit, params: { id: question.id } }
-
-      it 'assign Edit Question' do
-        expect(assigns(:question)).to eq question
-      end
+    it 'assign answer ' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+    it 'assign answer attachment' do
+      expect(assigns(:answer).attachments.first).to be_a_new(Attachment)
     end
 
-    context 'user is authorized', js: true do
-      context 'attach' do
-        login_user
-        it 'assign Attach' do
-          expect(assigns(:answer).attachments.first).to be_a_new(Attachment)
-        end
-      end
-      context 'user not is not owner' do
-        before do
-          sign_in @other_user
-          get :edit, params: { id: question.id, format: :js }
-        end
-        it 'assign Edit Question' do
-          expect(assigns(:question)).to eq question
-        end
-      end
-
-      context 'user is owner' do
-        before do
-          sign_in @user
-          get :edit, params: { id: question.id, format: :js }
-        end
-        it 'assign Edit Question' do
-          expect(assigns(:question)).to eq question
-        end
-      end
-    end
   end
 
   describe 'GET #new' do
@@ -91,199 +52,100 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'POST #create' do
+    let(:form_params) { {} }
     let(:params) do
       {
-        question: attributes_for(:question)
+        question: attributes_for(:question).merge(form_params)
       }
     end
 
-    context 'user is not authorized' do
-      it 'add tot database' do
-        expect { post :create, params: params }.to_not change(Question, :count)
+    subject { post :create, params: params }
+
+    it_behaves_like 'when user is unauthorized' do
+      it { expect { subject }.to_not change(Question, :count) }
+      it { expect(subject).to redirect_to new_user_session_path }
+    end
+
+    it_behaves_like 'when user is authorized' do
+      it { expect { subject }.to change(Question, :count) }
+
+      it_behaves_like 'invalid params concern', 'empty body', model: Question do
+        let(:form_params) { { body: '' } }
       end
-      it 'redirect to show' do
-        post :create, params: params
-        expect(response).to redirect_to new_user_session_path
+      it_behaves_like 'invalid params concern', 'empty title', model: Question do
+        let(:form_params) { { title: '' } }
       end
     end
-    context 'user is authorized' do
-      before { sign_in @other_user }
 
-      describe 'POST #create' do
-        context 'attr is valid' do
-          it 'add to database' do
-            expect { post :create, params: params, format: :json }.to change(@other_user.questions, :count).by(1)
-          end
-        end
-
-        context 'attr is not valid' do
-          let(:wrong_params) do
-            {
-              question: attributes_for(:wrong_question),
-              format: :js
-            }
-          end
-
-          it 'does not add to database' do
-            expect { post :create, params: wrong_params, format: :json }.to_not change(Question, :count)
-          end
-
-          it 'render new' do
-            post :create, params: wrong_params, format: :json
-            { title: 'b' * 6, body: 'b' * 61 }
-          end
-        end
-      end
-    end
   end
 
   describe 'PATCH update' do
-    let(:new_attr) do
-      {
-        title: 'b' * 5,
-        body: 'b' * 61
-      }
-    end
+    let(:form_params) do {
+      title: 'b' * 6,
+      body: 'z' * 6
+    } end
 
     let(:params) do
       {
         id: question.id,
-        question:     { title: new_attr[:title], body: new_attr[:body] },
-        format: :js
+        format: :js,
+        question: attributes_for(:question).merge(form_params)
       }
     end
-    let(:wrong_params) do
-      {
-        id: question.id,
-        question:     { title: nil, body: nil },
-        format: :js
-      }
-    end
-    context 'user is not authorized' do
-      let(:attr) do
-        { title: 'b' * 6, body: 'b' * 61 }
-      end
 
-      before do
-        patch :update, params: params
-        question.reload
-      end
-
-      it 'change attributes' do
-        expect(question.title).to_not eql new_attr[:title]
-        expect(question.body).to_not eql new_attr[:body]
-      end
+    subject do
+      patch :update, params: params
+      question.reload
     end
 
-    context 'user is authorized' do
-      context 'user user is not owner' do
-        before { sign_in @other_user }
+    it_behaves_like 'when user is unauthorized' do
+      before { subject }
+      it { expect(question.body).to_not eql params[:question][:body] }
+      it { expect(question.title).to_not eql params[:question][:title] }
+    end
 
-        let(:attr) do
-          { title: 'b' * 6, body: 'b' * 61 }
-        end
-        before do
-          patch :update, params: params
-          question.reload
-        end
+    it_behaves_like 'when user not is owner' do
+      before { subject }
+      it { expect(question.body).to_not eql params[:question][:body] }
+      it { expect(question.title).to_not eql params[:question][:title] }
+    end
 
-        it 'change attributes' do
-          expect(question.title).to_not eql new_attr[:title]
-          expect(question.body).to_not eql new_attr[:body]
-        end
+    it_behaves_like 'when user is authorized' do
+      before { subject }
+      it { expect(question.body).to eql params[:question][:body] }
+      it { expect(question.title).to eql params[:question][:title] }
+      it { expect(question).to render_template :update }
 
-        it 'render show template' do
-          expect(response).to be_forbidden
-        end
+      it_behaves_like 'invalid params js', 'empty title', model: Question do
+        let(:form_params) { { body: '' } }
+        before { subject }
       end
-
-      context 'user is owner' do
-        before { sign_in @user }
-
-        context 'attr is valid' do
-          let(:attr) do
-            { title: 'b' * 6, body: 'b' * 61 }
-          end
-          before do
-            patch :update, params: params
-            question.reload
-          end
-
-          it 'change attributes' do
-            expect(question.title).to eql new_attr[:title]
-            expect(question.body).to eql new_attr[:body]
-          end
-
-          it 'render show template' do
-            expect(response).to render_template :update
-          end
-        end
-
-        context 'attr is not valid' do
-          let(:attr) do
-            { title: 'b', body: 'b' }
-          end
-          before do
-            patch :update, params: wrong_params
-            question.reload
-          end
-
-          it 'does not change attributes' do
-            expect(question.title).to_not eql wrong_params[:title]
-            expect(question.body).to_not eql wrong_params[:body]
-          end
-
-          it 'render edit template' do
-            expect(response).to render_template :update
-          end
-        end
+      it_behaves_like 'invalid params js', 'empty title', model: Question do
+        let(:form_params) { { title: '' } }
+        before { subject }
       end
     end
   end
 
   describe 'DELETE #destroy' do
-    context 'user is not authorized' do
-      let!(:question) { create(:question) }
+    let!(:question) { create(:question, user: user) }
 
-      it 'delete in DB' do
-        expect { delete :destroy, params: { id: question.id } }.to_not change { Question.count }
-      end
-
-      it 'redirect to index' do
-        delete :destroy, params: { id: question.id }
-        expect(response).to redirect_to new_user_session_path
-      end
+    subject do
+      delete :destroy, params: { id: question.id }
     end
-    context 'user is authorized' do
-      context 'user is not owner' do
-        before { sign_in @other_user }
+    it_behaves_like 'when user is unauthorized' do
+      it {  expect { subject }.to_not change { Question.count } }
+      it { expect(subject).to redirect_to new_user_session_path }
+    end
 
-        let!(:question) { create(:question, user: @user) }
+    it_behaves_like 'when user not is owner' do
+      it {  expect { subject }.to_not change { Question.count } }
+      it { expect(subject).to redirect_to root_path }
+    end
 
-        it 'delete in DB' do
-          expect { delete :destroy, params: { id: question.id } }.to_not change { Question.count }
-        end
-
-        it 'redirect to index' do
-          delete :destroy, params: { id: question.id }
-          expect(response).to redirect_to root_path
-        end
-      end
-
-      context 'user is owner' do
-        before { sign_in @user }
-
-        let!(:question) { create(:question, user: @user) }
-
-        it 'delete in DB' do
-          expect { delete :destroy, params: { id: question.id } }.to change { Question.count }.by(-1)
-        end
-
-        it 'redirect to index' do
-          delete :destroy, params: { id: question.id }
-          expect(response).to redirect_to questions_path
-        end
-      end
+    it_behaves_like 'when user is authorized' do
+      it { expect { subject }.to change { Question.count }.by(-1) }
+      it { expect(subject).to redirect_to questions_path }
     end
   end
 end
--- spec/shared_examples/controllers/invalid_examples.rb
+++ spec/shared_examples/controllers/invalid_examples.rb
@@ -14,6 +14,22 @@ shared_examples 'invalid params' do |message, model: nil, code: 422|
   end
 end
 
+shared_examples 'invalid params concern' do |message, model: nil, code: 403|
+  context message do
+    if model
+      it "doesn't create #{model}" do
+        expect { subject }.not_to change(model, :count)
+      end
+    end
+
+    if code
+      it "returns #{code}" do
+        expect(subject).to have_http_status(code)
+      end
+    end
+  end
+end
+
 shared_examples 'invalid params js' do |message, model: nil|
   context message do
     if model
