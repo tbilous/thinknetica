79b5267933be0c66ac96bd86f84de58385ed4b06
tbilous <tbilous@yahoo.com>
2016-12-12 12:07:02 +0200

    worker

--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -1,7 +1,7 @@
 class DailyMailer < ApplicationMailer
   def digest(user)
     @user = user
-    @questions= Question.daily_questions(Date.current)
+    @questions = Question.daily_questions(Date.yesterday)
     mail to: user.email if @questions.present?
   end
 end
