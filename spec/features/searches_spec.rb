ab2148866fb4d277f845e52d6a8ea591781ee8b7
Taras Bilous <tbilous@yahoo.com>
2016-12-22 15:39:53 +0200

    acceptance test

---
+++ spec/features/searches_spec.rb
@@ -0,0 +1,101 @@
+require 'sphinx_acceptance_helper'
+
+feature 'Search', %(
+  In order to be able to search resource
+  As an user
+  I'd like to be able to search for resources
+) do
+  include_context 'users'
+
+  let!(:question_01) { create(:question, title: 'Question with search', user: user) }
+  let!(:answer_01) { create(:answer, body: 'Answer with search') }
+  let!(:comment_01) { create(:comment,
+                             body: 'Comment with search',
+                             commentable_type: 'Question',
+                             commentable_id: question_01.id
+  ) }
+  let!(:comment_02) { create(:comment,
+                             body: 'Comment with search',
+                             commentable_type: 'Answer',
+                             commentable_id: answer_01.id
+  ) }
+
+  # resources that will not search
+  # let!(:question) { create(:question, user: user) }
+  # let!(:answer) { create(:answer, question: question, user: user) }
+  # let!(:comment) { create(:comment,
+  #                         commentable_type: 'Question',
+  #                         commentable_id: question_01.id
+  # ) }
+
+  # let!(:shadow_user) { john }
+
+  background do
+    index
+    visit(root_path)
+    fill_in 'q', with: 'search'
+  end
+
+  scenario 'can search from all resources', :js do
+    select 'All', from: 'object'
+    click_button 'Search'
+
+    within '.searches-list' do
+      expect(page).to have_selector('.search-item', count: 5)
+      # expect(page).to have_content(question_01.title)
+      # expect(page).to have_content(answer_01.body)
+      # expect(page).to have_content(comment_01.body)
+      # expect(page).to have_content(comment_02.body)
+      # expect(page).to have_content(user.email)
+    end
+  end
+
+=begin
+  scenario 'can search from questions', :js do
+    select 'Question', from: 'object'
+    click_button 'Search'
+
+    within '.searches-list' do
+      expect(page).to have_selector('.search-item', count: 1)
+      expect(page).to have_content(question_01.title)
+    end
+  end
+=end
+
+=begin
+  scenario 'can search answers', :js do
+    select 'Answer', from: 'object'
+    click_button 'Search'
+
+    within '.searches-list' do
+      expect(page).to have_selector('.search-item', count: 1)
+      expect(page).to have_content(answer_01.body)
+    end
+  end
+=end
+
+=begin
+  scenario 'can search comments', :js do
+    select 'Comment', from: 'object'
+    click_button 'Search'
+
+    within '.searches-list' do
+      expect(page).to have_selector('.search-item', count: 2)
+      expect(page).to have_content(comment_01.body)
+      expect(page).to have_content(comment_02.body)
+    end
+  end
+=end
+
+=begin
+  scenario 'can search users', :js do
+    select 'Author', from: 'object'
+    click_button 'Search'
+
+    within '.searches-list' do
+      expect(page).to have_selector('.search-item', count: 1)
+      expect(page).to have_content(user.email)
+    end
+  end
+=end
+end
