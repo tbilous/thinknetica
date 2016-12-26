42d036ed444a203cf1d33185c9fd9f80b057ba6e
Taras Bilous <tbilous@yahoo.com>
2016-12-26 18:11:17 +0200

    active cable allow

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://51.15.57.143/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.allowed_request_origins = ['http://51.15.57.143/']
 
