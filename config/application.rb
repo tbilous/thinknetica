17df8bec280e974d36a60968d286513035b5a509
Taras Bilous <tbilous@yahoo.com>
2016-12-29 22:01:14 +0200

    ActionCable setup

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Thinknetica
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.action_cable.disable_request_forgery_protection = false
+    config.action_cable.disable_request_forgery_protection = true
     config.active_job.queue_adapter = :sidekiq
 
     config.generators do |g|
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,9 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://5110fc8c-295e-4333-8ccd-255ed97bd087.pub.cloud.scaleway.com//cable'
+  # config.action_cable.url = 'wss://5110fc8c-295e-4333-8ccd-255ed97bd087.pub.cloud.scaleway.com//cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'http://5110fc8c-295e-4333-8ccd-255ed97bd087.pub.cloud.scaleway.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
